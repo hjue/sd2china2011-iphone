dd8630c117fd66fb375e5d4ac40d323363012eba
jason <haojue@gmail.com>
2011-11-24 10:50:44 +0800

    Initial Commit

---
+++ sd2china2011/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sd2china2011
+//
+//  Created by Yu Hao on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
